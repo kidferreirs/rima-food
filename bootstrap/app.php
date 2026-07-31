7e9a050f78a91b5c2e17af52add36f81046d82b4
Amir Ferreira <kid.ferreira@hotmail.com>
2026-07-31 09:40:38 -0300

    fix: registra middleware de restaurante

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LoadRestaurant;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+
+        $middleware->alias([
+            'restaurante' => LoadRestaurant::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
